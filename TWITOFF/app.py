648eb1e8d41d3f35d551ebcbc597ae5fb12b089f
mmastin <matthew.mastin@gmail.com>
2019-12-17 13:45:03 -0500

    day 2 work

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,6 +1,8 @@
 """Code for app"""
 
-from flask import Flask
+from flask import Flask, render_template, request
+from decouple import config
+from .models import DB, User
 
 # make our app factory
 
@@ -8,13 +10,17 @@ def create_app():
     app = Flask(__name__)
 
     # add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+    # stop tracking modifications on sqlalchemy config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
     return app
 
