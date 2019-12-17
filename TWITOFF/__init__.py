3f20ec0a5ea22828786673a5517ecdd8f41f59d8
mmastin <matthew.mastin@gmail.com>
2019-12-17 08:12:03 -0500

    basic setup work

---
+++ TWITOFF/__init__.py
@@ -0,0 +1,5 @@
+"""entry point for twitoff flask app"""
+
+from .app import create_app
+
+APP = create_app()
--- hello.py
+++ hello.py
@@ -7,4 +7,11 @@ app = Flask(__name__)
 
 # now define a function
 def hello():
-    return 'Hello World!'
+    return render_template('home.html')
+
+# make a second route
+@app.route('/about')
+
+# now make a function that goes with about
+def preds():
+    return render_template('about.html')
