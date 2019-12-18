b550dfee4210c86296f4747124ec25836c706695
mmastin <matthew.mastin@gmail.com>
2019-12-18 13:12:42 -0500

    day 3 work

--- TWITOFF/twitter.py
+++ TWITOFF/twitter.py
@@ -13,3 +13,23 @@ TWITTER = tweepy.API(TWITTER_AUTH)
 BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
 # to do - add functions
+def add_or_update_user(username):
+    """add or update a user and their tweets, or else give error"""
+    try:
+        twitter_user=TWITTER.get_user(username)
+        db_user = (User.query.get(twitter_user.id) or User(id=twitter.id, name=username))
+        DB.session.add(db_user)
+        tweets = twitter_user.timeline(count=200, exclude_replies=True, include_rts=False, tweet_mode='extended', since_id=db_user.newest_tweet_id)
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+        for tweet in tweets:
+            # calculate embedding on full tweet
+            embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300], embedding=embedding)
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
+    except Exception as e:
+        print(f'error processing {username}: {e}')
+        raise e
+    else:
+        DB.session.commit()
